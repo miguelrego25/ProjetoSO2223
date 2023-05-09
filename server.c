b5bda9e6eff5af8065d71fbee96850f8fe0cc8d6
miguelrego25 <miguel.m.rego@gmail.com>
2023-05-09 19:57:08 +0100

    not so initial commit

--- server.c
+++ server.c
@@ -1,43 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <sys/wait.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <sys/time.h>
-
-#define BUFF_SIZE 256
-typedef struct{
-    int pid;
-    char program_name[BUFF_SIZE];
-    struct timeval start_time;
-}ProgramInfo;
-
-int main(){
-    int fd;
-    char buff[BUFF_SIZE];
-    char *prog_info[BUFF_SIZE];
-
-    for (int i = 0; i < BUFF_SIZE; i++) {
-        prog_info[i] = NULL;
-    }
-
-    mkfifo("server_fifo",0666);
-    printf("server walking...\n");
-
-    while(1){
-        fd = open("server_fifo",O_RDONLY);
-        read(fd,buff,BUFF_SIZE);
-
-        //ver o que foi recebido e comparar atraves de if clauses
-        if(strncmp(buff,"execute ",))
-    }
-}
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 //#include <sys/types.h>
 //#include <sys/stat.h>
 #include <sys/wait.h>
@@ -63,7 +26,7 @@ int main(int argc, char* argv){
     pid_t pid = fork();
 
     if(pid == 0){
-        
+
         //ler do pipe a informação
         int fd= open("server_Fifo", O_RDONLY);
 
@@ -86,10 +49,9 @@ int main(int argc, char* argv){
     }else{
         close("server_Fifo");
     }
-  
-  return 0;
+
+    return 0;
 
 
 }
-
 
